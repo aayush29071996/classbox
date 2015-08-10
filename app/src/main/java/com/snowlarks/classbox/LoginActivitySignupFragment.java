6e031a573a535b1f91102b07649808d3201119de
Saswat <saswatds@gmail.com>
2015-08-10 19:03:41 +0530

    Create Signin & Signup Fragments

--- app/src/main/java/com/snowlarks/classbox/LoginActivitySignupFragment.java
+++ app/src/main/java/com/snowlarks/classbox/LoginActivitySignupFragment.java
@@ -9,14 +9,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class LoginActivityFragment extends Fragment {
+public class LoginActivitySignupFragment extends Fragment {
 
-    public LoginActivityFragment() {
+    public LoginActivitySignupFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_login_signup, container, false);
     }
 }
